e5a0dabe89001f677ab11c0a9da1b9a79f5553e8
QUANG HIEU <quanghieuhg2005@gmail.com>
2026-03-11 14:21:22 +0700

    refactor: rename
    rename

--- ems/src/main/java/com/group5/ems/dto/request/SaveUserRequest.java
+++ ems/src/main/java/com/group5/ems/dto/request/SaveUserRequest.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class SaveUserRequestDTO {
+public class SaveUserRequest {
 
     private Long id;
     private String username;
