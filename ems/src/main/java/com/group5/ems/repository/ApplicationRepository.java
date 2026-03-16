ae8d231f117fdf9f82437357f9ffc3d84d0c1ac3
Dung Tran <trantrungd83@gmail.com>
2026-03-16 10:11:12 +0700

    resolve conflict

--- ems/src/main/java/com/group5/ems/repository/ApplicationRepository.java
+++ ems/src/main/java/com/group5/ems/repository/ApplicationRepository.java
@@ -17,15 +17,9 @@ public interface ApplicationRepository extends JpaRepository<Application, Long> 
 
     List<Application> findByJobPostIdAndStatus(Long jobPostId, String status);
 
-<<<<<<< Updated upstream
-    Application findByTrackingToken(String trackingToken);
-}
-=======
     int countByStatus(String status);
-
 
     Optional<Application> findByTrackingToken(String token);
 
     void deleteByTrackingToken(String token);
 }
->>>>>>> Stashed changes
